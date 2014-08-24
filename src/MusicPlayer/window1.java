392ec2e5314bddfe9c899bca8a1c2680c57c43eb
yash2710 <13bce123@nirmauni.ac.in>
2014-08-24 11:27:21 +0530

    n

---
+++ src/MusicPlayer/window1.java
@@ -0,0 +1,15 @@
+package MusicPlayer;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+
+public class window1 extends JFrame {
+	public window1() {
+		
+	}
+
+}
