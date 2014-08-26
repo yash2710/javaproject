1e565c64ae37048bb709cff13178c8aabe67f00e
yash2710 <13bce123@nirmauni.ac.in>
2014-08-26 19:55:41 +0530

    j

---
+++ src/MusicPlayer/Library.java
@@ -0,0 +1,17 @@
+package MusicPlayer;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+
+public class Library extends JFrame{
+	JPanel library() {
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		
+		return panel;
+	}
+		
+}
